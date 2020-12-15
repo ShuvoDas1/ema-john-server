74f95d39ec3ab40449c9177b78b04f78eaa69e23
ShuvoDas1 <shuvo0381@gmail.com>
2020-12-15 14:05:22 +0600

    file upload done

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const express = require('express')
 require('dotenv').config()
-const  cors = require('cors')
+const  cors = require('cors');
+const fs = require('fs-extra');
 const bodyParser= require('body-parser')
 const MongoClient = require('mongodb').MongoClient;
+const fileUpload = require('express-fileupload')
 
 
-console.log(process.env.DB_USER);
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vktpy.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true,useUnifiedTopology: true  });
 
 
-const app = express()
-app.use(cors())
-app.use(bodyParser.json())
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static('products'));
+app.use(fileUpload());
+
 const port = 4000
 
 
@@ -23,18 +28,42 @@ client.connect(err => {
 
 
     app.post('/addProduct',(req,res) =>{
-        const products = req.body;
-        productsCollection.insertMany(products)
-        .then(result =>{
-            console.log(result.insertedCount);
-            res.send(result.insertedCount)
+        const file = req.files.file;
+        const name = req.body.name;
+        const stock = req.body.stock;
+        const seller = req.body.seller;
+        const price = req.body.price;
+        const filePath = `${__dirname}/products/${file.name}`;
+        
+         console.log(seller, stock);
+        file.mv(filePath, err => {
+            if(err){
+              console.log(err);
+               res.status(500).send({msg: 'Failed to upload image'})
+            }
+            const newImg = fs.readFileSync(filePath);
+            const encImg = newImg.toString('base64');
+            let image = {
+              contentType: req.files.file.mimetype,
+              size: req.files.file.size,
+              img: Buffer.from(encImg, 'base64')
+            } 
+            productsCollection.insertOne({name, price, seller, stock, image})
+            .then(result =>{
+                fs.remove(filePath, error =>{
+                    console.log(error)
+                    res.send(result. insertedCount > 0)
+                })
+            })          
         })
+
 
     })
 
     app.get('/products',(req,res) =>{
-
-      productsCollection.find({})
+      const search = req.query.search;
+      console.log(search);
+      productsCollection.find({name: {$regex:search} })
       .toArray((err,documents) =>{
         res.send(documents)
       })
